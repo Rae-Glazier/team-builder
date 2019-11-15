3dc22cfd3a42aed1cf8c7a34b1213ff4082c192e
rae-glazier <glazier.home.life@gmail.com>
2019-11-14 19:34:18 -0600

    still working on debugging

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
 const Form = props => {
+    // console.log(props.teams);
+
     return (
         <div className='team-list'>
-            {props.forms.map(form => (
-                <div className='form' key={form.id}>
-                    <h2>{form.title}</h2>
-                    <p>{form.body}</p>
-                </div>
+            {props.teams.map(form => (
+                console.log(form.name)
+                // <div className='form' key={form.id}>
+                //     <h2>{form.name}</h2>
+                //     <p>{form.email}</p>
+                //     <p>{form.role}</p>
+                // </div>
             ))}
         </div>
     );
