540bd59d0d81630c40918927a8e1a2c407094eb8
rae-glazier <glazier.home.life@gmail.com>
2020-02-26 11:49:58 -0600

    cleaning up code

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import TeamMembers from './TeamMembers';
-import Form from './Form';
 import { useState } from 'react'
 
-function App() {
+import TeamMembers from './TeamMembers';
+import Form from './Form';
+
+const App = () => {
   const [teams, setTeams] = useState([{
-    id: 1,
-    name: 'Rae Glazier',
-    email: 'glazier.home.life@gmail.com',
-    role: 'Junior Web Dev'
+    name: '',
+    email: '',
+    role: ''
   }
 ]);
 
@@ -26,23 +25,11 @@ const addNewForm = form => {
 
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      
       <h1>My Team</h1>
       <Form addNewForm={addNewForm} />
       <TeamMembers teams={teams} />
+
     </div>
   );
 }
